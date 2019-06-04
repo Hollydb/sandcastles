faba5be7c24be2cf4947eb84a6ae76151cf94384
Student <example@example.com>
2019-06-04 13:38:13 -0700

    update

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,30 @@ $(document).ready(function() {
     $("body").addClass("red-background");
   });
 });
+$(document).ready(function() {
+  $("div#sandcastle1").click(function() {
+    $("body").removeClass();
+    $("body").addClass("purple");
+
+  });
+
+  $("div#sandcastle2").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+    $("body").fadetoggle();
+  });
+
+  $("div#sandcastle3").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+    $("body").fadetoggle();
+  });
+});
+$(document).ready(function() {
+  $("h1").click(function() {
+    $("#sandcastle2").slideToggle();
+    $("#sandcastle1").slideToggle();
+    $("#sandcastle3").slideToggle();
+  });
+
+});
